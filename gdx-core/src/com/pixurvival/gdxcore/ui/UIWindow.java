8812478cf58e16f5148d0dc90fe0f734badb8a40
SharkHendrix <joannick.gardize@gmail.com>
2019-06-02 18:28:38 +0200

    blablabla

--- gdx-core/src/com/pixurvival/gdxcore/ui/UIWindow.java
+++ gdx-core/src/com/pixurvival/gdxcore/ui/UIWindow.java
@@ -2,7 +2,6 @@ package com.pixurvival.gdxcore.ui;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.pixurvival.gdxcore.PixurvivalGame;
-import com.pixurvival.gdxcore.ScreenResizeEvent;
 
 public class UIWindow extends Window {
 
@@ -24,22 +23,5 @@ public class UIWindow extends Window {
 
 	public float getCenterY() {
 		return getY() + getHeight() / 2;
-	}
-
-	private void moveOnScreenResize(ScreenResizeEvent event) {
-		if (getCenterX() < event.getPrevScreenWidth() / 2) {
-			float ratio = getX() / event.getPrevScreenWidth();
-			setX(ratio * event.getNewScreenWidth());
-		} else {
-			float ratio = (event.getPrevScreenWidth() - getX() - getWidth()) / event.getPrevScreenWidth();
-			setX(event.getNewScreenWidth() - ratio * event.getNewScreenWidth() - getWidth());
-		}
-		if (getCenterY() < event.getPrevScreenHeight() / 2) {
-			float ratio = getY() / event.getPrevScreenHeight();
-			setY(ratio * event.getNewScreenHeight());
-		} else {
-			float ratio = (event.getPrevScreenHeight() - getY() - getHeight()) / event.getPrevScreenHeight();
-			setY(event.getNewScreenHeight() - ratio * event.getNewScreenHeight() - getHeight());
-		}
 	}
 }
