a13cf1e34f52e6c8456a6993788446febc907811
Allyson-B <beaucourt.allyson@hotmail.fr>
2019-06-05 20:29:39 +0200

    Ajout du panel consommable dans le content pack (pas fini)

--- contentpack-editor/main/src/com/pixurvival/contentPackEditor/component/item/EdiblePanel.java
+++ contentpack-editor/main/src/com/pixurvival/contentPackEditor/component/item/EdiblePanel.java
@@ -1,22 +1,42 @@
 package com.pixurvival.contentPackEditor.component.item;
 
+import javax.swing.JPanel;
+
+import com.pixurvival.contentPackEditor.component.effect.AlterationEditor;
+import com.pixurvival.contentPackEditor.component.util.LayoutUtils;
 import com.pixurvival.contentPackEditor.component.valueComponent.Bounds;
 import com.pixurvival.contentPackEditor.component.valueComponent.DoubleInput;
+import com.pixurvival.contentPackEditor.component.valueComponent.ListEditor;
+import com.pixurvival.contentPackEditor.component.valueComponent.VerticalListEditor;
+import com.pixurvival.contentPackEditor.event.EventManager;
+import com.pixurvival.core.contentPack.item.EdibleItem;
+import com.pixurvival.core.livingEntity.alteration.Alteration;
+import com.pixurvival.core.livingEntity.alteration.InstantDamageAlteration;
 
 public class EdiblePanel extends ItemSpecificPartPanel {
 
 	private static final long serialVersionUID = 1L;
 
+	private ListEditor<Alteration> alterationsEditor = new VerticalListEditor<>(AlterationEditor::new, InstantDamageAlteration::new, ListEditor.HORIZONTAL, false);
+	private DoubleInput durationField = new DoubleInput(Bounds.min(0));
+
 	public EdiblePanel() {
 
-		// TODO
-		DoubleInput durationField = new DoubleInput(Bounds.min(0));
+		EventManager.getInstance().register(this);
+
+		JPanel propertiesPanel = LayoutUtils.createVerticalLabelledBox("generic.duration", durationField);
+		propertiesPanel.setBorder(LayoutUtils.createGroupBorder("generic.properties"));
+
+		alterationsEditor.setBorder(LayoutUtils.createGroupBorder("effectTargetEditor.alterations"));
+		LayoutUtils.addVertically(this, propertiesPanel, alterationsEditor);
 
 	}
 
 	@Override
 	public void bindTo(ItemEditor itemEditor) {
-		// TODO
+		itemEditor.bind(durationField, EdibleItem::getDuration, EdibleItem::setDuration, EdibleItem.class);
+		itemEditor.bind(alterationsEditor, EdibleItem::getAlterations, EdibleItem::setAlterations, EdibleItem.class);
+
 	}
 
 }
